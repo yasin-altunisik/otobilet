33e8c937049789451b2d186c01230094433d2a02
Yasin Altunisik <altun.yasin.isik@gmail.com>
2023-04-15 17:00:37 +0300

    initial setups and entry screen added

--- App.js
+++ App.js
@@ -1,20 +1,91 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import {
+  StyleSheet,
+  Text,
+  View,
+  TextInput,
+  SafeAreaView,
+  StatusBar,
+  TouchableOpacity,
+} from "react-native";
 
 export default function App() {
+  const [mail, onChangeMail] = React.useState("Please enter your mail here.");
+  const [password, onChangePassword] = React.useState(
+    "Please enter your password here."
+  );
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView style={styles.container}>
+      <StatusBar backgroundColor="crimson" />
+      <View style={styles.headerContainer}>
+        <Text style={styles.header}>Otobilet</Text>
+      </View>
+      <View style={styles.bodyContainer}>
+        <Text>
+          Otobilet dünyasına hoşgeldiniz! Kampanyalar ve özel indirimlerden
+          faydalanmak için giriş yapın.
+        </Text>
+        <Text> </Text>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeMail}
+          value={mail}
+        />
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangePassword}
+          value={password}
+        />
+        <TouchableOpacity style={styles.button}>
+          <Text>GİRİŞ</Text>
+        </TouchableOpacity>
+        <Text> </Text>
+        <Text>veya</Text>
+        <Text> </Text>
+        <TouchableOpacity style={styles.button}>
+          <Text>KAYDOL</Text>
+        </TouchableOpacity>
+      </View>
+    </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  },
+  input: {
+    height: 40,
+    width: 250,
+    margin: 12,
+    borderBottomWidth: 1,
+    padding: 10,
+  },
+  headerContainer: {
+    flex: 1,
+    backgroundColor: "crimson",
+    justifyContent: "center",
+  },
+  header: {
+    textAlign: "center",
+    fontSize: 50,
+    fontWeight: "bold",
+    color: "white",
+  },
+  bodyContainer: {
+    padding: 20,
+    flex: 2,
+    backgroundColor: "white",
+    width: "100%",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  button: {
+    width: 100,
+    backgroundColor: "grey",
+    padding: 5,
+    borderRadius: 5,
+    textAlign: "center",
   },
 });
